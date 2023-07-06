11bc7b25ec55c561b982e8212f7c8087a7404c4d
Deds ID <81201449+DedsID@users.noreply.github.com>
2023-07-06 09:02:19 +0000

    Update 10 files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
-      'laptop': '1340px',
-      'ataslaptop': '1920px',
+        laptop: "1340px",
+        ataslaptop: "1920px",
       },
       colors: {
-        'bg' : '#F6F6F6'
-      }
+        bg: "#F6F6F6",
+        text: "#222222",
+        secol: "#b7b7b7",
+      },
     },
   },
   plugins: [],
-}
-
+};
